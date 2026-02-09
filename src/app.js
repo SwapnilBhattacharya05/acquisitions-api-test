a71d8e0f659729be003da65ae1d23604eae3fd46
Swapnil Bhattacharya <swapnilb050203@gmail.com>
2026-02-09 14:15:07 +0530

    Revert "Implement authentication"
    This reverts commit cde31d06eb50475926ffe2aad079ee4e64b75022.

--- src/app.js
+++ src/app.js
@@ -4,7 +4,6 @@ import helmet from "helmet";
 import morgan from "morgan";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-import authRoute from "#routes/auth.route.js";
 
 const app = express();
 
@@ -21,28 +20,10 @@ app.use(cookieParser()); // For parsing cookies that are sent in the headers of 
  */
 app.use(morgan("combined", {stream: {write: (message) => logger.info(message.trim())}}));
 
-// Test Endpoint
+
 app.get('/', (req, res) => {
     logger.info("Hello from Acquisitions API!"); // Log a message to indicate that the endpoint was hit
     res.status(200).send('Hello from Acquisitions!');
 });
 
-/**
- * Health and Readiness Checks
- * These endpoints later can be used by Kubernetes or any other orchestration tool to check the health and readiness of the application.
- */
-
-// Health Check Endpoint
-app.get('/health', (req, res) => res.status(200).json({
-    status: "ok",
-    timestamp: new Date().toISOString(),
-    uptime: process.uptime()
-}))
-
-// Readiness Check Endpoint
-app.get("/api", (req, res) => res.status(200).json({status: "ok"}));
-
-
-// Routes
-app.use("/api/auth", authRoute); // /api/auth/sign-up, /api/auth/sign-in, etc.
 export default app;
